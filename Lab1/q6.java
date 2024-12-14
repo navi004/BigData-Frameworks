eb3dbd924b5954a292853633084237100a51a05f
Naveen Nidadavolu <115283282+navi004@users.noreply.github.com>
2024-12-14 11:12:57 +0530

    Update q6.java

--- Lab1/q6.java
+++ Lab1/q6.java
@@ -1,1 +1,32 @@
 //6. Find largest and smallest number in an array in java
+import java.util.Scanner;
+import java.util.Arrays;
+class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        
+        int n= in.nextInt();
+        int arr[] = new int[n];
+        
+        for(int i=0;i<n;i++){
+            arr[i]=in.nextInt();
+        }
+        
+        System.out.println(Arrays.toString(arr));
+        
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        
+        for(int i=0;i<n;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+            if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+        
+        System.out.println("MIN: "+min+"\nMAX: "+max);
+        
+    }
+}
